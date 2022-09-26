f42b56c549b9c2cabe99bd99528993b74e347be3
axel7083 <42176370+axel7083@users.noreply.github.com>
2022-09-26 18:53:43 -0400

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,8 @@
+module Lib
+    ( construireWave
+    ) where
+
+----
+-- Votre code démarre dans la fonction suivante :
+construireWave :: String -> String -> String -> [Int]
+construireWave instruments tracks composition = [1]
