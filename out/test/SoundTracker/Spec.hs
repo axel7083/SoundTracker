30ed349c6ac0edb996a89044151298c0c147578c
axel7083 <42176370+axel7083@users.noreply.github.com>
2022-09-27 22:21:45 -0400

    Creating some functions

--- out/test/SoundTracker/Spec.hs
+++ out/test/SoundTracker/Spec.hs
@@ -1,7 +1,53 @@
 main :: IO ()
 main = do
-   contents <- readFile "./test/instruments.txt"
-   print $ lines contents
+   -- orders <- readFile "./test/instruments.txt"
+   -- First extracts the lines of order
+   -- let raw_lines = lines orders
+   -- print (read (head raw_lines) :: Double)
+   print $ runTests tests 0
+   
+
+abc :: [String]
+abc = ["A", "B", "C"]
+
+instrumentsSample :: [String]
+instrumentsSample = ["instrument"] ++ abc ++ ["instrument", "D"]
+   
+tests :: [Bool]
+tests = [
+    -- Test relative to the getChildren function
+    getChildren "instrument" instrumentsSample ==  abc,
+    null (getChildren "instrument" ["instrument"]),
+    members (head $ decompose "instrument" instrumentsSample 0) == abc,
+    members (decompose "instrument" instrumentsSample 0 !! 1) == ["D"]
+  ]
+
+-- This function will ensure all the assert in tests are true, otherwise will raise an error
+runTests :: [Bool] -> Int -> Bool
+runTests [] _ = True
+runTests (x : xs) i | x = runTests xs (i + 1)
+                    | otherwise = error ("The test at index " ++ show i ++ " failed")
+
+
+-- This method will take two argument, a stop condition and an array
+-- Example getChildren ( "S" ["A", "B", "S", "C"] ) = ["A", "B"]
+getChildren :: String -> [String] -> [String]
+getChildren _ [] = []
+getChildren stop (x : xs) | stop == x = []
+                          | otherwise = x : getChildren stop xs
+
+data Group = Group 
+  {
+    index :: Int
+  , members :: [String]
+  }
+
+
+decompose :: String -> [String] -> Int -> [Group]
+decompose _ [] _ = []
+decompose condition (x : xs) index  | x == condition = Group 1 (getChildren condition xs) : decompose condition xs (index + 1)
+                                    | otherwise = decompose condition xs index
+
 
 
 
